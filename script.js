40eafbcca692d4099e95f15ebec9570868374c73
romainpiti <bokuwa.romaiiin@gmail.com>
2026-06-18 18:41:01 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,9 +1,169 @@
 const seal = document.getElementById("seal");
 const envelope = document.getElementById("envelope");
 
-console.log("JS chargé");
-
 seal.addEventListener("click", () => {
-    console.log("clic !");
-    envelope.classList.toggle("open");
+    envelope.classList.add("open");
+});
+
+const closeBtn = document.getElementById("closeLetter");
+
+closeBtn.addEventListener("click", () => {
+    envelope.classList.remove("open");
+});
+
+
+/* ---------- NOTEBOOK ---------- */
+
+const notebook = document.getElementById("notebook");
+const notebookPage = document.getElementById("notebookPage");
+const closeNotebook = document.getElementById("closeNotebook");
+
+notebook.addEventListener("click", () => {
+    notebookPage.style.display = "block";
+});
+
+closeNotebook.addEventListener("click", () => {
+    notebookPage.style.display = "none";
+});
+
+
+/* ---------- POEMS ---------- */
+
+const poems = [
+{
+    title: "The flower",
+
+    text: `In the middle of a flower field,
+One shines brightly among the others.
+
+She is beautiful like a rose,
+She is brave like an iris,
+She is strong like a gladiolus,
+She is sweet like a cornflower.
+
+Every flower around is looking at her,
+Her name is Eve.`
+},
+
+{
+    title: "Test",
+
+    text: `Coming soon...`
+}
+];
+
+const poemList =
+    document.getElementById("poemList");
+
+poems.forEach((poem, index) => {
+
+    const li =
+        document.createElement("li");
+
+    li.textContent =
+        poem.title;
+
+    li.addEventListener("click", () => {
+        openPoem(index);
+    });
+
+    poemList.appendChild(li);
+
+});
+
+const summaryPage =
+    document.getElementById("summaryPage");
+
+const poemPage =
+    document.getElementById("poemPage");
+
+const poemTitle =
+    document.getElementById("poemTitle");
+
+const poemText =
+    document.getElementById("poemText");
+
+const backButton =
+    document.getElementById("backToSummary");
+
+
+function openPoem(index) {
+
+    poemTitle.textContent =
+        poems[index].title;
+
+    poemText.textContent =
+        poems[index].text;
+
+    summaryPage.style.display = "none";
+
+    poemPage.style.display = "block";
+}
+
+
+backButton.addEventListener("click", () => {
+
+    poemPage.style.display = "none";
+
+    summaryPage.style.display = "block";
+
+});
+
+const drawings = [
+
+{
+    title: "You",
+    date: "18 June 2026",
+    image: "YOU.jpg"
+},
+
+{
+    title: "For You",
+    date: "18 June 2026",
+    image: "doggierose.png"
+}
+
+];
+
+const box =
+    document.getElementById("box");
+
+const boxPage =
+    document.getElementById("boxPage");
+
+const closeBox =
+    document.getElementById("closeBox");
+
+const drawingGallery =
+    document.getElementById("drawingGallery");
+
+
+box.addEventListener("click", () => {
+
+    boxPage.style.display = "block";
+
+});
+
+
+closeBox.addEventListener("click", () => {
+
+    boxPage.style.display = "none";
+
+});
+
+drawings.forEach(drawing => {
+
+    const card =
+        document.createElement("div");
+
+    card.classList.add("drawing-card");
+
+    card.innerHTML = `
+        <img src="${drawing.image}">
+        <h3>${drawing.title}</h3>
+        <p>${drawing.date}</p>
+    `;
+
+    drawingGallery.appendChild(card);
+
 });
